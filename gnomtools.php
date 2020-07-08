b5907b92d93599ea78a3f7d9c913721c18fc2005
Dirk <dirkthomasmulder@gmail.com>
2020-07-08 20:05:28 +0700

    Added updates

--- gnomtools.php
+++ gnomtools.php
@@ -185,15 +185,15 @@ body {
 	text-align: center;
 	margin-bottom: 5%;
 	width: 100%;
-	height: 25px;
+	height: 55px;
 	/* left: 50%; */
 	/* position: relative; */
 	/* left: 50%; */
 	background: url(https://i.imgur.com/CtWtTdS.png) !important;
-	background-position: center;
-	background-size: 190px;
+	background-position: center !important;
+	background-size: 190px !important;
 	content: '';
-	background-repeat: no-repeat;
+	background-repeat: no-repeat !important;
 }
 
 .login .button-primary {
@@ -286,3 +286,25 @@ input[type="color"], input[type="date"], input[type="datetime-local"], input[typ
     </style>
 <?php }
 add_action( 'login_enqueue_scripts', 'wpb_login_logo' );
+
+
+include_once('updater.php');
+
+
+
+if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+	$config = array(
+		'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+		'proper_folder_name' => 'gnomtools', // this is the name of the folder your plugin lives in
+		'api_url' => 'https://api.github.com/repos/dirk789/GNOM-Tools', // the GitHub API url of your GitHub repo
+		'raw_url' => 'https://raw.github.com/dirk789/GNOM-Tools/master', // the GitHub raw url of your GitHub repo
+		'github_url' => 'https://github.com/dirk789/GNOM-Tools', // the GitHub url of your GitHub repo
+		'zip_url' => 'https://github.com/dirk789/GNOM-Tools/zipball/master', // the zip url of the GitHub repo
+		'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+		'requires' => '3.0', // which version of WordPress does your plugin require?
+		'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+		'readme' => 'README.md', // which file to use as the readme for the version number
+		'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+	);
+	new WP_GitHub_Updater($config);
+}
